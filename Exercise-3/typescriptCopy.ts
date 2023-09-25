756c19eb0865d987c26be9ced768c3e65938008d
hilindst <114443718+hilindst@users.noreply.github.com>
2023-09-25 17:22:26 -0500

    Exercise 2
    typescript exercises

---
+++ Exercise-3/typescriptCopy.ts
@@ -0,0 +1,17 @@
+typescriptCopy code
+interface Vehicle {
+    brand: string;
+    speed: number;
+    
+    accelerate(): void;
+}
+
+const car: Vehicle = {
+    brand: "Toyota",
+    speed: 120,
+    accelerate(): void {
+        console.log(`The ${this.brand} is accelerating`);
+    }
+}
+
+car.accelerate();
